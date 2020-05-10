fb2c19b9ab59be72de38846ce707bb5a3a4d2eae
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-05-10 12:55:14 +0200

    Assessments ViewModel

---
+++ DermaHelp/ViewModels/AssessmentsViewMode/AssessmentViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  AssessmentViewModel.swift
+//  DermaHelp
+//
+//  Created by Mohamed Shiha on 5/10/20.
+//  Copyright © 2020 Mohamed Shiha. All rights reserved.
+//
+
+import struct Foundation.Date
+import class UIKit.UIImage
+
+protocol AssessmentViewModelDelegate: class {
+    
+}
+
+protocol AssessmentModelViewType {
+    init(assessment: Assessment)
+    var organ: String { get }
+    var status: Assessment.Status { get }
+    var date: Date { get }
+    var riskRate: Float { get }
+    var nevusRate: Float { get }
+    var melanomaRate: Float { get }
+    var colorRate: Float { get }
+    var attachedImage: UIImage? { get }
+}
+
+class AssessmentViewModel: AssessmentModelViewType {
+    
+    private var assessment: Assessment
+    weak var delegate: AssessmentViewModelDelegate?
+    
+    required init(assessment: Assessment) {
+        self.assessment = assessment
+    }
+    
+    var organ: String {
+        return assessment.organ
+    }
+    
+    var status: Assessment.Status {
+        return assessment.status
+    }
+    
+    var date: Date {
+        return assessment.date
+    }
+    
+    var riskRate: Float {
+        return assessment.riskRate
+    }
+    
+    var nevusRate: Float {
+        return assessment.nevusRate
+    }
+    
+    var melanomaRate: Float {
+        return assessment.melanomaRate
+    }
+    
+    var colorRate: Float {
+        return assessment.colorRate
+    }
+    
+    var attachedImage: UIImage? {
+        return assessment.attachedImage
+    }
+}
