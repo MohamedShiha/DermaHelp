5fd3f827be1dc7d1105685ee2d4116dddb9d79a3
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-05-11 11:56:37 +0200

    Changed help text and layout.

--- DermaHelp/Views/Composite Views/ManualView.swift
+++ DermaHelp/Views/Composite Views/ManualView.swift
@@ -14,7 +14,7 @@ class ManualView: UIView, LayoutController {
     // MARK: Views
     
     private lazy var titleLabel = Label(text: "How it works", font: .roundedSystemFont(ofSize: 20, weight: .bold))
-    private lazy var manualLabel = Label(text: "DermaHelp analyses your captured images in a highly intelligent way to provide the most accurate results, raises your awareness, and shows you the nearest dermatologist on the map.", font: .roundedSystemFont(ofSize: 17), numberOfLines: 0)
+    private lazy var manualLabel = Label(text: "DermaHelp analyses your captured photos in a highly intelligent way to provide you with the most accurate results, raises your awareness, and shows you the nearest dermatologist on the map.", font: .roundedSystemFont(ofSize: 17), numberOfLines: 0)
     
     // MARK: Initializers
     
@@ -46,6 +46,6 @@ class ManualView: UIView, LayoutController {
         titleLabel.edgesToSuperView(including: [.top, .left, .right], insets: .top(8) + .left(16) + .right(12))
         manualLabel.layBelow(titleLabel, constant: 8)
         manualLabel.alignLeft(with: titleLabel, constant: 0)
-        manualLabel.edgesToSuperView(including: [.bottom, .right], insets: .bottom(16) + .right(16))
+        manualLabel.edgesToSuperView(including: [.bottom, .right], insets: .bottom(16) + .right(10))
     }
 }
