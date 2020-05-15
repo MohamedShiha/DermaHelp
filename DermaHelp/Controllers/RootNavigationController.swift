30fae06eb590191f51d85b7434de6376172c4e78
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-05-15 22:36:34 +0200

    Application root navigation controller.

---
+++ DermaHelp/Controllers/RootNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  RootNavigationController.swift
+//  DermaHelp
+//
+//  Created by Mohamed Shiha on 5/14/20.
+//  Copyright © 2020 Mohamed Shiha. All rights reserved.
+//
+
+import UIKit
+
+class RootNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.isHidden = true
+    }
+}
