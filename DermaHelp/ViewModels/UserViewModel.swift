810973ab0f55cc00da26197e65131a4931a23e81
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-05-17 02:54:57 +0200

    UserViewModel to populate user data.

---
+++ DermaHelp/ViewModels/UserViewModel.swift
@@ -0,0 +1,91 @@
+//
+//  UserViewModel.swift
+//  DermaHelp
+//
+//  Created by Mohamed Shiha on 5/16/20.
+//  Copyright © 2020 Mohamed Shiha. All rights reserved.
+//
+
+import UIKit
+
+protocol UserViewModelDelegate: class {
+    
+}
+
+class UserViewModel {
+    
+    private var user: User
+    weak var delegate: UserViewModelDelegate?
+    
+    init(user: User) {
+        self.user = user
+    }
+    
+    var id: String {
+        return user.id
+    }
+    
+    var name: String {
+        set {
+            user.name = newValue
+        }
+        get {
+            return user.name
+        }
+    }
+    
+    var email: String {
+        set {
+            user.email = newValue
+        }
+        get {
+            return user.email
+        }
+    }
+    
+    var picture: UIImage? {
+        set {
+            user.picture = newValue
+        }
+        get {
+            return user.picture
+        }
+    }
+    
+    var birthDate: Date? {
+        set {
+            user.birthDate = newValue
+        }
+        get {
+            return user.birthDate
+        }
+    }
+    
+    var gender: User.Gender? {
+        set {
+            user.gender = newValue
+        }
+        get {
+            return user.gender
+        }
+    }
+    
+    var assessments: [Assessment] {
+        set {
+            user.assessments = newValue
+        }
+        get {
+            return user.assessments
+        }
+    }
+    
+    var updatedAt: Date {
+        return user.updatedAt
+    }
+    
+    static func fetchCurrentUser(completion: @escaping (_ user: User?) -> Void) {
+        FirestoreManager.shared.getCurrentUser { (user) in
+            completion(user)
+        }
+    }
+}
