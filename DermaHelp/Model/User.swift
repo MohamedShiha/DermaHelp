37e4cd081586021f73b328e02edf8a04d16864cc
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-05-18 20:18:36 +0200

    Fixed encoding and decoding errors and changed assessments array to hold assessment identifiers.

--- DermaHelp/Model/User.swift
+++ DermaHelp/Model/User.swift
@@ -21,37 +21,33 @@ struct User: Codable {
     var picture: UIImage?
     var birthDate: Date?
     var gender: Gender?
-    var assessments: [Assessment]
+    var assessmentIds: [String]
     let updatedAt: Date
     
-    init(id: String, name: String, email: String, picture: UIImage?, birthDate: Date?, gender: Gender?, assessments: [Assessment], updatedAt: Date) {
+    enum CodingKeys: String, CodingKey {
+        case id, name, email, picture, assessmentIds = "assessments", birthDate, gender, updatedAt
+    }
+    
+    init(id: String, name: String, email: String, picture: UIImage?, birthDate: Date?, gender: Gender?, assessmentIds: [String], updatedAt: Date) {
         self.id = id
         self.name = name
         self.email = email
         self.picture = picture
         self.birthDate = birthDate
         self.gender = gender
-        self.assessments = assessments
+        self.assessmentIds = assessmentIds
         self.updatedAt = updatedAt
     }
     
-    enum CodingKeys: String, CodingKey {
-        case id, name, email, picture, assessments, birthDate, gender, updatedAt
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encode(email, forKey: .email)
-        try container.encode(picture?.encodeToBase64() ?? nil, forKey: .picture)
-        try container.encode(birthDate, forKey: .birthDate)
-        try container.encode(gender?.rawValue, forKey: .gender)
-        try container.encode(DateFormatter.shortFormat.string(from: updatedAt), forKey: .updatedAt)
-        var assessmentsContainer = container.nestedUnkeyedContainer(forKey: .assessments)
-        for assessment in assessments {
-            try assessmentsContainer.encode(assessment)
-        }
+    init() {
+        id = ""
+        name = ""
+        email = ""
+        picture = nil
+        birthDate = nil
+        gender = nil
+        assessmentIds = [String]()
+        updatedAt = Date()
     }
     
     init(from decoder: Decoder) throws {
@@ -62,12 +58,28 @@ struct User: Codable {
         picture = try? container.decode(String.self, forKey: .picture).decodeToImage()
         birthDate = try? container.decode(Date.self, forKey: .birthDate)
         gender = try? container.decode(Gender.self, forKey: .gender)
-        updatedAt = try container.decode(String.self, forKey: .updatedAt).decodeToDate() ?? Date()
-        var assessmentsContainer = try container.nestedUnkeyedContainer(forKey: .assessments)
-        var assessments = [Assessment]()
+        updatedAt = try container.decode(Date.self, forKey: .updatedAt)
+        var assessmentsContainer = try container.nestedUnkeyedContainer(forKey: .assessmentIds)
+        var ids = [String]()
         while !assessmentsContainer.isAtEnd {
-            assessments.append(try assessmentsContainer.decode(Assessment.self))
+            let id = try assessmentsContainer.decode(String.self)
+            ids.append(id)
         }
-        self.assessments = assessments
+        self.assessmentIds = ids
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+        try container.encode(email, forKey: .email)
+        try container.encode(picture?.encodeToBase64() ?? nil, forKey: .picture)
+        try container.encode(birthDate, forKey: .birthDate)
+        try container.encode(gender?.rawValue, forKey: .gender)
+        try container.encode(updatedAt, forKey: .updatedAt)
+        var assessmentsContainer = container.nestedUnkeyedContainer(forKey: .assessmentIds)
+        for id in assessmentIds {
+            try assessmentsContainer.encode(id)
+        }
     }
 }
