cf311b726b7d412e85a261fdc52d37398561bd14
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-06-07 12:15:06 +0200

    Added credentials evaluator.

--- DermaHelp/Extensions/String+Extensions.swift
+++ DermaHelp/Extensions/String+Extensions.swift
@@ -18,12 +18,48 @@ extension String {
         return UIImage(data: data)
     }
     
-    func decodeToDate() -> Date? {
-        return DateFormatter.shortFormat.date(from: self)
-    }
-    
     func usernameFromEmail() -> String {
         let segments = split(separator: "@")
         return segments.count > 1 ? String(segments.first ?? "") : ""
+    }
+    
+    private func evaluate(text: String, by regex: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+        return predicate.evaluate(with: text)
+    }
+    
+    func evaluateEmail() -> Bool {
+        // This regex matches the string that has at least a character
+        // then @ then a character then . then a character,
+        // whitespace and special characters are forbidden
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        return evaluate(text: self, by: regex)
+    }
+    
+    func evaluatePassword() -> Bool {
+        // This regex matches the string that at least contains: 1 digit, 1 uppercase,
+        // 1 lowercase letter and 8 characters at least and 24 characters at most,
+        // whitespace and special characters are forbidden
+        let regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,24}$"
+        return evaluate(text: self, by: regex)
+    }
+}
+
+extension Array where Element == String {
+    var areEmpty: Bool {
+        var isEmpty = true
+        self.forEach { (string) in
+            isEmpty = isEmpty && string.isEmpty
+        }
+        return isEmpty
+    }
+    
+    var areEqual: Bool {
+        var isEqual = true
+        for var i in 0..<self.count - 1 {
+            isEqual = isEqual && self[i].elementsEqual(self[i+1])
+            i += 1
+        }
+        return isEqual
     }
 }
