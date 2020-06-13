63d08a5de4973c162103b510c01255675fe84b91
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-06-13 18:58:46 +0200

    Changed the appearance and layout of the AlertController.

--- DermaHelp/Base Components/Controllers/AlertController.swift
+++ DermaHelp/Base Components/Controllers/AlertController.swift
@@ -11,12 +11,18 @@ import EZConstraints
 
 class AlertController: ViewController, LayoutController {
 
+    enum Action {
+        case primary
+        case secondary
+        case neutral
+    }
+    
     // MARK: Views
 
     private let blurView: UIVisualEffectView = {
         let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
         effectView.contentView.backgroundColor = UIColor(white: 1, alpha: 0.8)
-        effectView.layer.cornerRadius = 12
+        effectView.layer.cornerRadius = 16
         effectView.clipsToBounds = true
         return effectView
     }()
@@ -24,14 +30,26 @@ class AlertController: ViewController, LayoutController {
     private let bodyLabel = Label(text: "Body", font: .roundedSystemFont(ofSize: 15), alignment: .center, numberOfLines: 0)
     private let stackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.spacing = 8
+        stackView.spacing = stackView.axis == .horizontal ? 4 : 8
         stackView.distribution = .fillEqually
         return stackView
     }()
     
+    // MARK: Properties
+    
+    var buttonStackAxis: NSLayoutConstraint.Axis {
+        set {
+            stackView.axis = newValue
+        }
+        get {
+            return stackView.axis
+        }
+    }
+    private let buttonHeight: CGFloat = 36
+    
     // MARK: Initializer
     
-    init(title: String?, message: String, buttonStackAxis: NSLayoutConstraint.Axis) {
+    init(title: String, message: String?, buttonStackAxis: NSLayoutConstraint.Axis) {
         super.init(nibName: nil, bundle: nil)
         titleLabel.text = title
         bodyLabel.text = message
@@ -62,51 +80,74 @@ class AlertController: ViewController, LayoutController {
     
     func setupLayout() {
         blurView.center()
-        blurView.widthAnchor(with: view, multiplier: 0.7)
+        blurView.widthAnchor(with: view, multiplier: 0.725)
         [titleLabel, bodyLabel].edgesToSuperView(including: [.left, .right],
-                                                 insets: .left(14) + .right(14))
-        stackView.edgesToSuperView(including: [.left, .right], insets: .left(12) + .right(12))
+                                                 insets: .left(18) + .right(18))
+        stackView.edgesToSuperView(including: [.left, .right], insets: .left(16) + .right(16))
         titleLabel.layTopInSuperView(constant: 20)
         bodyLabel.layBelow(titleLabel, constant: 6)
         stackView.layBelow(bodyLabel, constant: 12)
         stackView.layBottomInSuperView(constant: 12)
         if stackView.axis == .horizontal {
-            stackView.heightAnchor(.equal, constant: 40)
+            stackView.heightAnchor(.equal, constant: buttonHeight)
         }
     }
     
-    private func alertButtonFactory(title: String?, alertStyle: UIAlertAction.Style) -> Button {
+    private func alertButtonFactory(title: String, alertStyle: UIAlertAction.Style, action: Action) -> Button {
         var weight: UIFont.Weight = .regular
-        var color = UIColor.systemBlue
+        var color = UIColor.black
+        var backColor = UIColor.clear
         switch alertStyle {
         case .cancel:
             weight = .semibold
         case .destructive:
-            color = .systemRed
+            backColor = .systemRed
         default:
             weight = .regular
         }
-        let button = Button(title: title, font: .roundedSystemFont(ofSize: 17, weight: weight), titleColor: color, backColor: .systemFill)
-        button.layer.cornerRadius = 8
+        switch action {
+        case .primary:
+            backColor = .mainTint
+            color = .white
+        case .secondary:
+            backColor = .systemFill
+        default:
+            backColor = .clear
+        }
+        let button = Button(title: title, font: .roundedSystemFont(ofSize: 17, weight: weight), titleColor: color, backColor: backColor)
+        button.layer.cornerRadius = 16
         return button
     }
     
     // MARK: Adding Alerts
     
-    func createAlert(title: String?, alertStyle: UIAlertAction.Style, handler: (() -> Void)? = nil) {
-        let button = alertButtonFactory(title: title, alertStyle: alertStyle)
+    func createAlert(title: String, action: Action, alertStyle: UIAlertAction.Style, handler: (() -> Void)? = nil) {
+        let button = alertButtonFactory(title: title, alertStyle: alertStyle, action: action)
         if stackView.axis == .vertical {
-            button.heightAnchor(.equal, constant: 36)
+            button.heightAnchor(.equal, constant: buttonHeight)
         }
-        stackView.addArrangedSubview(button)
+        if stackView.axis == .horizontal {
+            stackView.addArrangedSubview(button)
+        } else if stackView.axis == .vertical {
+            stackView.insertArrangedSubview(button, at: 0)
+        }
+        if stackView.arrangedSubviews.count > 1 {
+            if stackView.axis == .horizontal {
+                stackView.arrangedSubviews.first?.backgroundColor = .clear
+            } else if stackView.axis == .vertical {
+                stackView.arrangedSubviews.last?.backgroundColor = .clear
+            }
+        }
         button.didTapButton = { (sender) in
             handler?()
         }
     }
     
-    class func dismissingAlert(title: String?, message: String, axis: NSLayoutConstraint.Axis = .horizontal, dismissingTitle: String) -> AlertController {
+    class func dismissingAlert(title: String, message: String?,
+                               axis: NSLayoutConstraint.Axis = .horizontal,
+                               dismissingTitle: String) -> AlertController {
         let alertController = AlertController(title: title, message: message, buttonStackAxis: axis)
-        alertController.createAlert(title: dismissingTitle, alertStyle: .cancel) {
+        alertController.createAlert(title: dismissingTitle, action: .secondary, alertStyle: .cancel) {
             alertController.dismiss(animated: true, completion: nil)
         }
         return alertController
