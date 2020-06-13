e43c377a35dc6461df7cbc77cb01d965872035b4
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-06-13 18:46:37 +0200

    Base Authentication Provider class.

---
+++ DermaHelp/Base Components/Networking/AuthenticationProvider.swift
@@ -0,0 +1,29 @@
+//
+//  AuthenticationProvider.swift
+//  DermaHelp
+//
+//  Created by Mohamed Shiha on 5/12/20.
+//  Copyright © 2020 Mohamed Shiha. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import GoogleSignIn
+
+class AuthenticationProvider {
+    
+    static let shared = AuthenticationProvider()
+    typealias OperationCompletion = (_ error: Error?) -> Void
+    typealias UserCompletion = (_ isSignedIn: Bool) -> Void
+    
+    @discardableResult
+    func userLoginState(completion: @escaping UserCompletion) -> AuthStateDidChangeListenerHandle {
+        return Auth.auth().addStateDidChangeListener { (_, user) in
+            if user == nil {
+                completion(false)
+            } else {
+                completion(true)
+            }
+        }
+    }
+}
