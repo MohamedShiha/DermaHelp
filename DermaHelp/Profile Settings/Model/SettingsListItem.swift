365ee1fe5bd3d8a9b5f90d75c2180dce3db0647f
Mohamed Shiha <mohamedshiha15@gmail.com>
2020-06-13 20:32:16 +0200

    A static list with variable data to be representted in Settings VIew

---
+++ DermaHelp/Profile Settings/Model/SettingsListItem.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsListItem.swift
+//  DermaHelp
+//
+//  Created by Mohamed Shiha on 6/8/20.
+//  Copyright © 2020 Mohamed Shiha. All rights reserved.
+//
+
+import UIKit
+
+struct SettingsListItem {
+    let name: String
+    var data: String?
+    let handler: () -> Void
+}
